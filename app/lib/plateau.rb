7e6213a377682a2a460d3538cfebbd8456a8d03c
Timothy Fenney <kodekata@gmail.com>
2019-02-06 13:26:23 -0500

    Update plateau to report passable locations

--- app/lib/plateau.rb
+++ app/lib/plateau.rb
@@ -1,12 +1,33 @@
-# Class Plateau
-#
-# This is FauxO programming, OO, except without mutation.
-# This implementation is bare in order to allow testing of
-# Builder with real collaborators.
+require 'set'
+
+class PositionNotUniqueError < StandardError
+end
 
 class Plateau
   def initialize(x_size:, y_size:)
     @x_size = x_size
     @y_size = y_size
+    @locations = Set.new
   end
+
+  def passable?(location:)
+    if location.x < 0 || location.x >= @x_size || location.y < 0 || location.y >= @y_size
+      return false
+    end
+
+    return false if @locations.include? location
+    
+    true
+  end
+
+  def register(location:)
+    raise PositionNotUniqueError if @locations.include? location
+    @locations.add(location)
+  end
+
+  def to_s
+    "<Plateau x_size=#{@x_size} y_size=#{@y_size}>"
+  end
+
+  attr_reader :x_size, :y_size
 end
--- spec/lib/plateau_spec.rb
+++ spec/lib/plateau_spec.rb
@@ -1,7 +1,83 @@
-require_relative '../../app/lib/plateau'
+require_relative '../../app/lib/plateau.rb'
+require_relative '../../app/lib/vector_2d.rb'
 
 describe Plateau do
-  it 'can be built' do
-    plateau = Plateau.new(x_size: 1, y_size: 2)
+  context 'the plateau is large' do
+    before do
+      @plateau = Plateau.new(x_size: 100, y_size: 101)
+      @location1 = Vector2d.new(x: 5, y: 6)
+      @location2 = Vector2d.new(x: 5, y: 5)
+    end
+
+    it 'reports interior coordinates as passable' do
+      expect(@plateau.passable?(location: Vector2d.new(x: 99, y: 0))).to be(true)
+    end
+
+    it 'reports negative x coordinates as not passable' do
+      expect(@plateau.passable?(location: Vector2d.new(x: -1, y: 0))).to be(false)
+    end
+    
+    it 'reports x coordinates greater than plateau x as not passable' do
+      expect(@plateau.passable?(location: Vector2d.new(x: 105, y: 0))).to be(false)
+    end
+
+    it 'reports x coordinates equal to plateau x as not passable' do
+      expect(@plateau.passable?(location: Vector2d.new(x: 100, y: 0))).to be(false)
+    end
+
+    it 'reports negative y coordinates as not passable' do
+      expect(@plateau.passable?(location: Vector2d.new(x: 0, y: -1))).to be(false)
+    end
+
+    it 'reports y coordinates greater than plateau y as not passable' do
+      expect(@plateau.passable?(location: Vector2d.new(x: 0, y: 105))).to be(false)
+    end
+
+    it 'reports y coordinates equal to plateau y as not passable' do
+      expect(@plateau.passable?(location: Vector2d.new(x: 0, y: 101))).to be(false)
+    end
+
+    it 'reports its x dimension size' do
+      expect(@plateau.x_size).to eq(100)
+    end
+
+    it 'reports the coordinates of a registered location as not passable' do
+      @plateau.register(location: @location1)
+      expect(@plateau.passable?(location: @location1)).to be(false)
+    end
+
+    context 'registering locations' do
+      it 'registers a single location' do
+        @plateau.register(location: @location1)
+      end
+
+      it 'registers a second location with unique coordinates' do
+        @plateau.register(location: @location1)
+        expect{@plateau.register(location: @location2)}.not_to raise_error
+      end
+
+      it 'does not register a second location with the same coordinates' do
+        @plateau.register(location: @location1)
+        expect{@plateau.register(location: @location1)}.to raise_error(PositionNotUniqueError)
+      end
+    end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

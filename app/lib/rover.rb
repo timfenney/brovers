c757a3359936e70b17044a485974820aaa9209f9
Timothy Fenney <kodekata@gmail.com>
2019-02-06 11:44:39 -0500

    Add behaviour: rover can perform commands.

--- app/lib/rover.rb
+++ app/lib/rover.rb
@@ -1,12 +1,67 @@
 # Class Rover
 #
-# This is FauxO programming, OO, except without mutation.
-# This implementation is bare in order to allow testing of
-# Builder with real collaborators.
+# This class simulates a rover. It uses FauxO, so doesn't mutate itself, but
+# rather returns a new rover for changes.
+
+require_relative './vector_2d'
+
+# Constants
+
+# Commands
+M = 'M'.freeze
+R = 'R'.freeze
+L = 'L'.freeze
+
+# Directions
+N = 'N'.freeze
+E = 'E'.freeze
+S = 'S'.freeze
+W = 'W'.freeze
 
 class Rover
+  DELTA = {
+    N => Vector2d.new(x:  0, y:  1),
+    E => Vector2d.new(x:  1, y:  0),
+    S => Vector2d.new(x:  0, y: -1),
+    W => Vector2d.new(x: -1, y:  0)
+  }.freeze
+
+  LEFT = {
+    N => W,
+    E => N,
+    S => E,
+    W => S
+  }.freeze
+
+  RIGHT = {
+    N => E,
+    E => S,
+    S => W,
+    W => N
+  }.freeze
+
   def initialize(location:, direction:)
     @location = location
     @direction = direction
   end
+
+  def run(commands:)
+    next_location, next_direction = @location, @direction
+    
+    commands.chars.each do |command|
+      if command == M
+        maybe_next_location = next_location
+        delta = DELTA[next_direction]
+        maybe_next_location += delta
+        next_location = maybe_next_location
+      elsif command == R || command == L
+        turn = command == R ? RIGHT : LEFT
+        next_direction = turn[next_direction]
+      end
+    end
+    
+    Rover.new(location: next_location, direction: next_direction)
+  end
+
+  attr_accessor :location, :direction
 end
--- spec/lib/rover_spec.rb
+++ spec/lib/rover_spec.rb
@@ -2,8 +2,103 @@ require_relative '../../app/lib/rover.rb'
 require_relative '../../app/lib/vector_2d.rb'
 
 describe Rover do
-  it 'can be built' do
-    location = Vector2d.new(x: 1, y: 2)
-    rover = Rover.new(location: location, direction: 'N')
+  context 'the rover faces north' do
+    before do
+      location = Vector2d.new(x: 1, y: 2)
+      @rover = Rover.new(location: location, direction: 'N')
+    end
+
+    it 'reports its location' do
+      expect(@rover.location).to eq(Vector2d.new(x: 1, y: 2))
+    end
+
+    it 'reports its direction' do
+      expect(@rover.direction).to eq('N')
+    end
+
+    it 'returns a rover moved forward' do
+      rover = @rover.run(commands: 'M')
+      expect(rover.location).to eq(Vector2d.new(x: 1, y: 3))
+    end
+
+    it 'returns a rover turned right' do
+      rover = @rover.run(commands: 'R')
+      expect(rover.direction).to eq('E')
+    end
+    
+    it 'returns a rover turned left' do
+      rover = @rover.run(commands: 'L')
+      expect(rover.direction).to eq('W')
+    end
+
+    it 'returns a rover for multiple commands' do
+      rover = @rover.run(commands: 'LLLMMRM')
+      expect([rover.location,
+              rover.direction]).to eq([Vector2d.new(x: 3, y: 1), 'S'])
+    end
+  end
+
+  context 'the rover faces east' do
+    before do
+      location = Vector2d.new(x: 1, y: 2)
+      @rover = Rover.new(location: location, direction: 'E')
+    end
+
+    it 'returns a rover moved forward' do
+      rover = @rover.run(commands: 'M')
+      expect(rover.location).to eq(Vector2d.new(x: 2, y: 2))
+    end
+
+    it 'returns a rover turned right' do
+      rover = @rover.run(commands: 'R')
+      expect(rover.direction).to eq('S')
+    end
+
+    it 'returns a rover turned left' do
+      rover = @rover.run(commands: 'L')
+      expect(rover.direction).to eq('N')
+    end
+  end
+  
+  context 'the rover faces south' do
+    before do
+      @rover = Rover.new(location: Vector2d.new(x: 1, y: 2), direction: 'S')
+    end
+
+    it 'returns a rover moved forward' do
+      rover = @rover.run(commands: 'M')
+      expect(rover.location).to eq(Vector2d.new(x: 1, y: 1))
+    end
+
+    it 'returns a rover turned right' do
+      rover = @rover.run(commands: 'R')
+      expect(rover.direction).to eq('W')
+    end
+
+    it 'returns a rover turned left' do
+      rover = @rover.run(commands: 'L')
+      expect(rover.direction).to eq('E')
+    end
+  end
+  
+  context 'the rover faces west' do
+    before do
+      @rover = Rover.new(location: Vector2d.new(x: 1, y: 2), direction: 'W')
+    end
+
+    it 'returns a rover moved forward' do
+      rover = @rover.run(commands: 'M')
+      expect(rover.location).to eq(Vector2d.new(x: 0, y: 2))
+    end
+
+    it 'returns a rover turned right' do
+      rover = @rover.run(commands: 'R')
+      expect(rover.direction).to eq('N')
+    end
+
+    it 'returns a rover turned left' do
+      rover = @rover.run(commands: 'L')
+      expect(rover.direction).to eq('S')
+    end
   end
 end
